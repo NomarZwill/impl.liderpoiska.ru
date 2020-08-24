b55ae10a06269e8a8b5dd0fda34e6be2fe6339b2
NomarZwill <artm@liderpoiska.ru>
2020-08-24 16:29:54 +0300

    service page upd

--- frontend/web/src/js/components/servicePage.js
+++ frontend/web/src/js/components/servicePage.js
@@ -20,6 +20,35 @@ export default class ServicePage{
       $('.prices_wrapper').removeClass('_compact');
     });
 
+    $('.service_description .read_more').on('click', function(e){
+      $('.service_description p').addClass('_active');
+      $('.service_description .read_more').addClass('_hidden');
+      $('html,body').animate({scrollTop:$('.service_description p').offset().top}, 400);
+    });
+
+    if ($('.subservice_listing_item').length > 5) {
+      $('.prices_wrapper .show_all').removeClass('_hidden');
+      $('.prices_wrapper').addClass('_compact');
+    }
+
+    $('.prices_wrapper .read_more').on('click', function(e){
+      $('.prices_wrapper .show_all').addClass('_hidden');
+      $('.prices_wrapper').removeClass('_compact');
+    });
+
+    $('.subservice_wrapper .collapse_wrapper').on('click', function(e){
+
+      if ($(e.target).hasClass('close_block')){
+        $('.subservice_wrapper .close_block').addClass('_hidden');
+        $('.subservice_wrapper .open_block').removeClass('_hidden');
+        $('.subservice_wrapper .subservice_listing').addClass('_compact');
+      } else if ($(e.target).hasClass('open_block')){
+        $('.subservice_wrapper .close_block').removeClass('_hidden');
+        $('.subservice_wrapper .open_block').addClass('_hidden');
+        $('.subservice_wrapper .subservice_listing').removeClass('_compact');
+      }
+    });
+
     var doctorsWrapper  = new Swiper('.doctors_wrapper', {
       slidesPerView: 'auto',
       spaceBetween: 24,
